e6fb146250d1528e83e5fe00459319255b5c80bd
Remix Workspace <>
2022-11-05 16:56:18 -0400

    Add baggage checkin and security functions

--- contracts/BaggageTracker.sol
+++ contracts/BaggageTracker.sol
@@ -22,6 +22,7 @@ contract BaggageTracker {
     struct Baggage {
         string id;
         uint256 last_scanned_timestamp;
+        string location;
         BaggageStatus status;
     }
 
@@ -30,6 +31,9 @@ contract BaggageTracker {
     Customer[] private customers;
     Baggage[] private baggage;
 
+    mapping (string => Baggage) private baggageMapping;
+
+    //The baggage official is allowed to create a new contract, providing the list of customers as the input
     constructor(string[] memory customer_ids) {
         baggageOfficial = msg.sender;
 
@@ -39,6 +43,37 @@ contract BaggageTracker {
                 Customer({id: customer_ids[i], status: CustomerStatus.CHECKIN})
             );
         }
+    }
+
+    function checkInBaggage(
+        string[] memory baggageIds,
+        string[] memory locations
+    ) public {
+
+        //Length of baggage IDs and their locations must be equal.
+        require(baggageIds.length == locations.length);
+
+
+        //Adds the baggage to the baggage array
+        for (uint256 i = 0; i < baggageIds.length; i++) {
+
+            Baggage memory b = Baggage({
+                    id: baggageIds[i],
+                    last_scanned_timestamp: block.timestamp,
+                    location: locations[i],
+                    status: BaggageStatus.CHECK_IN
+                });
+
+            baggage.push(b);
+
+            baggageMapping[baggageIds[i]] = b;
+        }
+    }
+
+    function addBaggageToSecurity(string memory baggageId) public {
+
+        baggageMapping[baggageId].status = BaggageStatus.SECURITY;
+        baggageMapping[baggageId].last_scanned_timestamp = block.timestamp;
     }
 
     function getCustomers() public view returns (Customer[] memory) {
